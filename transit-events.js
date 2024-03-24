d01a50ec5f12c4d64feb734650d9bcead20a40a1
15HayHay <hayley@princesspausch>
2024-03-23 21:28:20 -0400

    Event update

--- transit-events.js
+++ transit-events.js
@@ -12,7 +12,11 @@
 const transitEvents= [
     {"start": "3/20/2024", "title" : "Reveal the Transit site", "description" : "Show everyone what I've been up to." } ,
     {"start": "3/20/2024", "title": "Technology Q&A" ,"description" :" Q & A about technology in the life of a Christian w/ Matt Ross"} ,
-    
+    {"start": "3/27/2024","title": "Colossians The Supremacy of Christ", "description":"Regular Transit meeting at the Loft"},
+    {"start": "4/3/2024","title": "No Transit", "description":"Happy Easter"},
+    {"start": "4/10/2024", "title": "Regular Transit meeting at The Loft", "description": ""},
+    {"start":"4/17/2024" ,"title": "Local church Q&A" , "description": ""},
+    {"start" :"4/24/2024", "title":""}
 ]
 
 

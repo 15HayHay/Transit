e3ecffa973524381cac45f5c8db10be74945597b
15HayHay <hayley@princesspausch>
2024-03-24 11:02:23 -0400

    Upated events

--- transit-events.js
+++ transit-events.js
@@ -12,11 +12,16 @@
 const transitEvents= [
     {"start": "3/20/2024", "title" : "Reveal the Transit site", "description" : "Show everyone what I've been up to." } ,
     {"start": "3/20/2024", "title": "Technology Q&A" ,"description" :" Q & A about technology in the life of a Christian w/ Matt Ross"} ,
-    {"start": "3/27/2024","title": "Colossians The Supremacy of Christ", "description":"Regular Transit meeting at the Loft"},
+    {"start": "3/27/2024","title": "Colossians The Supremacy of Christ", "description":"Regular Transit meeting at The Loft"},
     {"start": "4/3/2024","title": "No Transit", "description":"Happy Easter"},
-    {"start": "4/10/2024", "title": "Regular Transit meeting at The Loft", "description": ""},
+    {"start": "4/10/2024", "title": "Regular meeting at The Loft", "description": ""},
     {"start":"4/17/2024" ,"title": "Local church Q&A" , "description": ""},
-    {"start" :"4/24/2024", "title":""}
+    {"start" :"4/24/2024", "title":"Regular meeting at The Loft", "description":""},
+    {"start": "5/1/2024", "title":"Regular meeting at The Loft","description":""},
+    {"start": "5/8/2024","title":"No Transit","description": "Off for Basics Conference"},
+    {"start": "5/15/2024","title":"Group potluck", "description" : "Potluck at The Loft"},
+    {"start":"5/22/2024","title":"Regular meeting at The Loft","description":""},
+    {"start":"5/29/2024","title":"No Transit","description":"Happy Memorial Day"},
 ]
 
 

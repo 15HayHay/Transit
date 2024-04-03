6191c8ddb9e1fdd44d59146da5c8e07bb5f87cc9
15HayHay <hayley@princesspausch>
2024-04-02 22:51:53 -0400

    New events

--- transit-events.js
+++ transit-events.js
@@ -14,7 +14,9 @@ const transitEvents= [
     {"start": "3/20/2024", "title": "Technology Q&A" ,"description" :" Q & A about technology in the life of a Christian w/ Matt Ross"} ,
     {"start": "3/27/2024","title": "Colossians The Supremacy of Christ", "description":"Regular Transit meeting at The Loft"},
     {"start": "4/3/2024","title": "No Transit", "description":"Happy Easter"},
+    {"start": "4/6/2024","title":"Bainbridge Swing Dance @ Family Life Center","description": "8:00-11:00 PM"},
     {"start": "4/10/2024", "title": "Regular meeting at The Loft", "description": ""},
+    {"start": "4/13/2024", "title":"Roller Skating @ Chargin Valley Roller Rink","description":" "},
     {"start":"4/17/2024" ,"title": "Local church Q&A" , "description": ""},
     {"start" :"4/24/2024", "title":"Regular meeting at The Loft", "description":""},
     {"start": "5/1/2024", "title":"Regular meeting at The Loft","description":""},
@@ -22,6 +24,7 @@ const transitEvents= [
     {"start": "5/15/2024","title":"Group potluck", "description" : "Potluck at The Loft"},
     {"start":"5/22/2024","title":"Regular meeting at The Loft","description":""},
     {"start":"5/29/2024","title":"No Transit","description":"Happy Memorial Day!"},
+
 ]
 
 
